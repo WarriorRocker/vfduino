252efe3c2e1b7d3ad8bce2cba31f0dfdc3f27285
WarriorRocker <warriorrocker@gmail.com>
2013-01-08 13:53:16 -0500

    Update xodusamp.ino
    Added amplifier gpo functions

--- xodusamp.ino
+++ xodusamp.ino
@@ -6,14 +6,13 @@
 #include <SPI.h> //include SPI.h library for the vfd
 #include <IRremote.h> //include IRremote.h library
 
-//const int slaveSelectPin = 10; //define additional spi pin for vfd
 const int slaveSelectPin = 0; //define additional spi pin for vfd
-//const int motorAPin = 4; //define first control pin for volume motor
-//const int motorBPin = 5; //define second control pin for volume motor
-//const int channelPin = 6; //define used to select 2 or 4 channel input
-//const int powerPin = 7; //define pin used to turn amp on and off
+const int motorAPin = 4; //define first control pin for volume motor
+const int motorBPin = 5; //define second control pin for volume motor
+const int channelPin = 6; //define used to select 2 or 4 channel input
+const int powerPin = 7; //define pin used to turn amp on and off
 const int irPin = 8; //define the data pin of the ir reciever
-//const int modePin = 21; //define pin to check for current power mode
+const int modePin = 21; //define pin to check for current power mode
 
 IRrecv irrecv(irPin); //setup the IRremote library in recieve mode
 decode_results results; //setup buffer variable for ir commands
@@ -22,18 +21,46 @@ unsigned long lastPress = 0; //variable for tracking the last keypress
 bool firstCommand = true; //set variable to clear display with first command
 int flowControl = 0; //setup variable to contain current cursor position
 
+int powerThreshold = 900;
+
 //built in screens (each screen uses 42 bytes of flash)
 prog_uchar screens[][41] PROGMEM = {
   "-- XodusAmp Redux ----By Travis Brown --",
-  "--  Arduino  VFD  ---- waiting for PC --",
+  "--  Arduino  VFD  ----By Travis Brown --",
 };
 
+prog_uchar lines[][21] PROGMEM = {
+  "     Amplifier On   ",
+  "    Amplifier Off   ",
+  "      Volume Up     ",
+  "     Volume Down    ",
+  "   2 Channel Input  ",
+  "   4 Channel Input  ",
+  "     Amp is Off     ",
+  "   No power to Amp  ",
+};
+
+char lineBuffer[21] = { "                    " };
+
 void setup() {
+  pinMode(modePin, INPUT);
+  pinMode(motorAPin, OUTPUT);
+  pinMode(motorBPin, OUTPUT);
+  pinMode(powerPin, OUTPUT);
+  pinMode(channelPin, OUTPUT);
+  
+  digitalWrite(motorAPin, 0);
+  digitalWrite(motorAPin, 0);
+  digitalWrite(powerPin, 0);
+  digitalWrite(channelPin, 0);
+  
   Serial.begin(9600); //open a serial connection
   irrecv.enableIRIn(); // start the ir receiver
   vfdInitialize(); //initialize and setup the samsung 20T202DA2JA vfd
-
   vfdDisplayScreen(1); //display the first screen (startup)
+  if (analogRead(modePin) < powerThreshold) {
+    vfdDisplayLinePause(8);
+  }
 }
 
 void loop() {
@@ -43,35 +70,63 @@ void loop() {
 }
 
 void remoteProcess() {
-  //keycodes setup for apple mini remote
   byte keyPress = 0;
   if (irrecv.decode(&results)) {
-    switch (results.value) {
-      case 2011275437: //play/pause button
-        keyPress = 65;
-        break;
-      case 2011283629: //menu button
-        keyPress = 66;
-        break;
-      case 2011254957: //volume up
-        keyPress = 67;
-        break;
-      case 2011246765: //volume down
-        keyPress = 68;
-        break;
-      case 2011271341: //track left
-        keyPress = 69;
-        break;
-      case 2011259053: //track right
-        keyPress = 70;
-        break;
-    }
+    keyPress = remoteProcessKeycode();
     if ((keyPress) && (millis() > (lastPress + 200))) {
-      Serial.print((char)keyPress);
-      lastPress = millis();
+      remoteProcessCommand(keyPress);
     }
     irrecv.resume();
   }
+}
+
+byte remoteProcessKeycode() {
+  //keycodes setup for apple mini remote
+  switch (results.value) {
+    case 2011275437: //play/pause button
+      return 65;
+    case 2011283629: //menu button
+      return 66;
+    case 2011254957: //volume up
+      return 67;
+    case 2011246765: //volume down
+      return 68;
+    case 2011271341: //track left
+      return 69;
+    case 2011259053: //track right
+      return 70;
+  }
+}
+
+void remoteProcessCommand(byte keyPress) {
+  if (analogRead(modePin) >= powerThreshold) {
+    if (digitalRead(powerPin)) {
+      switch (keyPress) {
+        case 65:
+          ampTogglePower();
+          break;
+        case 66:
+          ampToggleChannelInput();
+          break;
+        case 67:
+          ampVolumeUp();
+          break;
+        case 68:
+          ampVolumeDown();
+          break;
+      }
+    } else {
+      if (keyPress == 65) {
+        ampTogglePower();
+      } else {
+        vfdDisplayLinePause(7);
+      }
+    }
+  } else {
+    vfdDisplayLinePause(8);
+  }
+  Serial.print((char)keyPress);
+  lastPress = millis();
 }
 
 void vfdInitialize() {
@@ -82,7 +137,7 @@ void vfdInitialize() {
   vfdCommand(0x01); //clear all display and set DD-RAM address 0 in address counter
   vfdCommand(0x02); //move cursor to the original position
   vfdCommand(0x06); //set the cursor direction increment and cursor shift enabled
-  vfdCommand(0x0c); //set display on,cursor on,blinking off
+  vfdCommand(0x0c); //set display on, cursor on, blinking off
   vfdCommand(0x38); //set 8bit operation, 2 line display, and 100% brightness level
   vfdCommand(0x80); //set cursor to the first position of 1st line 
 }
@@ -95,17 +150,20 @@ void vfdProcess() {
       firstCommand = false;
     }
     byte rxbyte = serialGet();  
-    if (rxbyte == 254) { //code 0xfd signals matrix orbital command
-      vfdProcessCommand(); //call function to process the pending command
+    if (rxbyte == 254) {
+      vfdProcessCommand();
     } else {
       if (flowControl == 20) {
         vfdCommand(0xc0); //set cursor to the first position of 2nd line 
       } else if (flowControl == 40) {
         vfdCommand(0x80); //set cursor to the first position of 1st line 
-        flowControl = 0; //reset flow control back to start position
+        flowControl = 0;
+      }
+      if (flowControl >= 20) {
+        lineBuffer[flowControl - 20] = vfdProcessData(rxbyte);
       }
       flowControl++;
-      vfdData(vfdProcessData(rxbyte)); //process the character and then send as data
+      vfdData(vfdProcessData(rxbyte));
     }
   }
 }
@@ -122,31 +180,33 @@ void vfdProcessCommand() {
       break;
     case 66: //backlight on (minutes)
       temp = serialGet();
-      vfdCommand(0x38); //set 8bit operation, 2 line display, and 100% brightness level
+      vfdCommand(0x38);
       break;
     case 70: //backlight off
-      vfdCommand(0x3b); //set 8bit operation, 2 line display, and 25% brightness level
+      vfdCommand(0x3b);
       break;
     case 71: //set cursor position (column, row)
       temp = (serialGet() - 1);
       flowControl = temp;
-      if (serialGet() == 2) {
-        temp += 0x40; //increase cursor position to the next line
+      switch (serialGet()) {
+      case 2:
+        temp += 0x40;
         flowControl += 20;
+        break;
       }
-      vfdCommand(0x80 + temp); //set cursor to the nth position
+      vfdCommand(0x80 + temp);
       break;
     case 72: //set cursor home
       flowControl = 0;
-      vfdCommand(0x80); //set cursor to the first position of 1st line 
+      vfdCommand(0x80);
       break;
     case 76: //move cursor left
       flowControl--;
-      vfdCommand(0x10); //set cursor position one character to the left
+      vfdCommand(0x10);
       break;
     case 77: //move cursor right
       flowControl++;
-      vfdCommand(0x11); //set cursor position one character to the right
+      vfdCommand(0x11);
       break;
     case 78: //define custom character (id, data 8 bytes)
       vfdCommand(0x40 + (serialGet() * 8));
@@ -154,11 +214,15 @@ void vfdProcessCommand() {
         vfdData(serialGet());
       }
       break;
-    case 86: //GPO off (number)
-      temp = serialGet();
-      break;
     case 87: //GPO on (number)
-      temp = serialGet();
+      switch (serialGet()) {
+        case 1:
+          ampTogglePower();
+          break;
+        case 4:
+          ampToggleChannelInput();
+          break;
+      }
       break;
     case 88: //clear display
       flowControl = 0;
@@ -181,6 +245,7 @@ void vfdProcessCommand() {
     case 82: //auto scroll off
     case 83: //show blinking block cursor
     case 84: //hide blinking block cursor
+    case 86: //GPO off (number)
     case 96: //auto-repeat mode off
     case 104: //init horizontal bar graph
     case 109: //init medium size digits
@@ -281,16 +346,40 @@ void vfdCommand(unsigned char temp_2) {
   digitalWrite(slaveSelectPin, HIGH);
 }
 
+byte vfdGetData(unsigned char temp) {
+  digitalWrite(slaveSelectPin, LOW);
+  //SPI.transfer(
+  temp = SPI.transfer(0xfb);
+  digitalWrite(slaveSelectPin, HIGH);
+  return temp;
+}
+
 void vfdDisplayScreen(int screen) {
   for (int i = 0; i < 20; i++) {
     vfdCommand(0x80 + i);
     vfdData(vfdProcessData(pgm_read_byte(&(screens[(screen - 1)][i]))));
-    vfdCommand(0xc0 + (19 - i));
-    vfdData(vfdProcessData(pgm_read_byte(&(screens[(screen - 1)][20 + (19 - i)]))));
+    vfdCommand(0xc0 + i);
+    lineBuffer[i] = vfdProcessData(pgm_read_byte(&(screens[(screen - 1)][20 + i])));
+    vfdData(lineBuffer[i]);
   }
 }
 
+void vfdDisplayLine(int line) {
+ for (int i = 0; i < 20; i++) {
+   vfdCommand(0xc0 + i);
+   vfdData(pgm_read_byte(&(lines[(line - 1)][i])));
+ } 
+}
+
+void vfdDisplayLinePause(int line) {
+  vfdDisplayLine(line);
+  delay(250);
+  vfdCopyFromLineBuffer();
+}
+
 void vfdWriteDebug(byte rxbyte) {
+  vfdCommand(0x01); //clear all display and set DD-RAM address 0 in address counter
+  vfdCommand(0x80); //set cursor to the first position of 1st line 
   char z_str[5] = "    ";
   itoa(rxbyte, z_str, 10);
   for (int i=0; i<5; i++) {
@@ -298,9 +387,59 @@ void vfdWriteDebug(byte rxbyte) {
   }
 }
 
+void vfdCopyFromLineBuffer() {
+ for (int i = 0; i < 20; i++) {
+   vfdCommand(0xc0 + i);
+   vfdData(lineBuffer[i]);
+ }
+}
+
 byte serialGet() {
   int incoming;
   while (!Serial.available()) { }
   incoming = Serial.read();
   return (byte) (incoming &0xff);
+}
+
+void ampVolumeUp() {
+  vfdDisplayLine(3);
+  digitalWrite(motorAPin, 1);
+  remoteRepeatWait();
+  digitalWrite(motorAPin, 0);
+  vfdCopyFromLineBuffer();
+}
+
+void ampVolumeDown() {
+  vfdDisplayLine(4);
+  digitalWrite(motorBPin, 1);
+  remoteRepeatWait();
+  digitalWrite(motorBPin, 0);
+  vfdCopyFromLineBuffer();
+}
+
+void ampToggleChannelInput() {
+  if (digitalRead(channelPin)) {
+    digitalWrite(channelPin, 0);
+    vfdDisplayLinePause(6);
+  } else {
+    digitalWrite(channelPin, 1);
+    vfdDisplayLinePause(5);
+  }
+}
+
+void ampTogglePower() {
+  if (digitalRead(powerPin)) {
+    digitalWrite(powerPin, 0);
+    vfdDisplayLinePause(2);
+  } else {
+    digitalWrite(powerPin, 1);
+    vfdDisplayLinePause(1);
+  }
+}
+
+void remoteRepeatWait() {
+  do {
+    irrecv.resume();
+    delay(150);
+  } while ((irrecv.decode(&results)) && (results.value == 4294967295));
 }
